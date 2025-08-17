7669887a854414b584b454385f59219d00ca766d
goncalof <x358280@bcpcorp.net>
2025-08-17 12:51:46 +0100

    removed lsp saga

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -29,6 +29,9 @@ return {
 			vim.keymap.set("n", "<leader>tc", function()
 				neotest.run.run()
 			end)
+			vim.keymap.set("n", "<leader>tf", function()
+				neotest.run.run(vim.fn.expand("%"))
+			end)
 		end,
 	},
 }
